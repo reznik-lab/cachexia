5ae72cb75e5cb7474172489de4b6fe6d10ccbf89
Amy Xie <104649789+amyyyxie@users.noreply.github.com>
2024-12-02 13:46:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -46,10 +46,8 @@ def main():
     df_episodes_all.to_csv(episodes_file, index=False)
     print("Analysis complete. Processed episodes saved to:", episodes_file)
 
-    
-    qc_results = quality_control(episodes_file, output_path)
     qc_results_file = output_path + 'quality_control_results.csv'
-    qc_results.to_csv(qc_results_file, index=False)
+    valid_episodes = quality_control(episodes_file, qc_results_file)
 
 if __name__ == "__main__":
     main()
